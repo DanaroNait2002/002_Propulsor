b9fac7d174de5066454c409c384250e37ce18fd7
Daniela Rodríguez <danaronait2002@gmail.com>
2022-10-06 12:27:04 +0100

    Avance en clase (Después del recreo)
    Más cosas xD

--- Propulsor/Assets/Scripts/PlayerController.cs
+++ Propulsor/Assets/Scripts/PlayerController.cs
@@ -2,36 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
+    //Vector de movimiento
     Rigidbody2D body;
     Vector2 direction;
 
+    //IMPULSE por defecto
     [SerializeField]
     float impulse = 50f;
 
+    //Texto de FUEL
     [SerializeField]
     TextMeshProUGUI labelFuel;
 
+    //PARTICLES de EXPLOSION 
     [SerializeField]
     GameObject prefabsParticles;
 
+    //Pantalla final DERROTA
+    [SerializeField]
+    GameObject finalScreemLost;
 
     //Variable tipo float de FUEL actual
+    [SerializeField]
     float fuel = 100f;
 
 
-    
+
+
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
-        
+
         //Fuerza inicial de BODY hacia la derecha
         //body.AddForce(transform.right, ForceMode2D.Impulse);
     }
 
-    
+
     void Update()
     {
         //Obtengo datos de movimiento en el mando/teclado
@@ -41,16 +51,25 @@ public class PlayerController : MonoBehaviour
         //El valor de FUEL disminuye con el tiempo
         fuel = fuel - 1 * Time.deltaTime;
         labelFuel.text = fuel.ToString("00") + "%";
+
+        if (fuel <= 0.0f)
+        {
+            //Desactivar el script
+            this.enabled = false;
+
+            //Activación de la pantalla final de DERROTA
+            finalScreemLost.SetActive(true);
+        }
     }
 
     //Recarga de combustible
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Fuel")
+        if (collision.tag == "Fuel" && fuel > 0.0f)
         {
             //Aumenta en 10 el FUEL
             fuel += 10f;
-            
+
             //Limitamos que el total sobrepase el 100%
             if (fuel > 100f)
             {
@@ -70,5 +89,11 @@ public class PlayerController : MonoBehaviour
     {
         //Empujo hacia donde se mueve
         body.AddForce(direction, ForceMode2D.Impulse);
+    }
+
+    public void ClickEnBoton()
+    {
+        Debug.Log("Ha clicado");
+        SceneManager.LoadScene("SampleScene");
     }
 }
